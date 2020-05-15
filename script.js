0e3c591c8893d468f552a67dd7704a3d377f0a7f
GabsLopes055 <51964786+GabsLopes055@users.noreply.github.com>
2020-05-14 21:26:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var socket = io('http://localhost:3000');
+
+function renderMessage(message) {
+    $('.messages').append('<div class = "message"><strong>' + message.author + '</strong>: ' + message.message + '</div>')
+}
+
+socket.on('previousMessages', function(messages) {
+    for (message of messages) {
+        renderMessage(message);
+    }
+});
+
+socket.on('receivedMessege', function(message) {
+    renderMessage(message);
+});
+
+$('#chat').submit(function(event) {
+    event.preventDefault();
+
+    var author = $('input[name=username]').val();
+    var message = $('input[name=message]').val();
+
+    if (author.length && message.length) {
+        var messageObject = {
+            author: author,
+            message: message,
+        };
+    } else {
+        alert('Informe um @User e digite sua @Menssage')
+    };
+
+    renderMessage(messageObject);
+
+    socket.emit('sendMessage', messageObject);
+
+    $('input[name=message]').val('');
+
+})
